4a51d8abaeda17a0d4d01564d65beef240cbf99a
Mannymin <Mannymin@gmail.com>
2014-05-19 13:56:17 -0400

    projects

--- night_lifeapp/app/models/friendgroup.rb
+++
@@ -1,2 +0,0 @@
-class Friendgroup < ActiveRecord::Base
-end
--- night_lifeapp/db/migrate/20140517200706_create_friendgroups.rb
+++
@@ -1,9 +0,0 @@
-class CreateFriendgroups < ActiveRecord::Migration
-  def change
-    create_table :friendgroups do |t|
-      t.int :user_id
-
-      t.timestamps
-    end
-  end
-end
